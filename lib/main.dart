8ef655a77fd6b4181d0c0f57f3fcbdadf2ce160a
Roberto Ribeiro <robertov82008@gmail.com>
2018-09-09 13:34:12 -0300

    104 - Definindo o Tema

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,51 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future main() async {
-  Firestore.instance
-      .collection('teste')
-      .document('teste')
-      .setData({'teste': 'teste'});
-
-  DocumentSnapshot getSnapshot =
-      await Firestore.instance.collection('teste').document('teste').get();
-  print("getSnapshot: ${getSnapshot.documentID}");
-
-  QuerySnapshot querySnapshot =
-      await Firestore.instance.collection('teste').getDocuments();
-
-  for (DocumentSnapshot doc in querySnapshot.documents) {
-    print("querySnapshot: ${doc.documentID}");
-  }
-
-  Firestore.instance.collection('teste').snapshots().listen((snapshot) {
-
-    for (DocumentSnapshot doc in snapshot.documents) {
-      print("listen: ${doc.data}");
-    }
-  });
-
+void main() {
   runApp(new MyApp());
 }
+
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Chat App',
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Chat App'),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+      ),
+    );
   }
 }
