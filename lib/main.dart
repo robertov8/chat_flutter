971207b8cc67b4b974bc5882a8bdfcedf31308e5
Roberto Ribeiro <robertov82008@gmail.com>
2018-09-09 00:35:30 -0300

    103 - Lendo os Dados do Banco

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future main() async {
   Firestore.instance
       .collection('teste')
       .document('teste')
       .setData({'teste': 'teste'});
+
+  DocumentSnapshot getSnapshot =
+      await Firestore.instance.collection('teste').document('teste').get();
+  print("getSnapshot: ${getSnapshot.documentID}");
+
+  QuerySnapshot querySnapshot =
+      await Firestore.instance.collection('teste').getDocuments();
+
+  for (DocumentSnapshot doc in querySnapshot.documents) {
+    print("querySnapshot: ${doc.documentID}");
+  }
+
+  Firestore.instance.collection('teste').snapshots().listen((snapshot) {
+
+    for (DocumentSnapshot doc in snapshot.documents) {
+      print("listen: ${doc.data}");
+    }
+  });
 
   runApp(new MyApp());
 }
